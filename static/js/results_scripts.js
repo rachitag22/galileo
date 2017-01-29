d22f90378013455c5e9c88960c3222f0b0570834
Rachit Agarwal <rachitagarwal0202@gmail.com>
2017-01-29 17:09:20 +0000

    a

--- static/js/results_scripts.js
+++ static/js/results_scripts.js
@@ -1,15 +1,23 @@
+var jsonURL = "https://daemon-dash-galileo-rachitag22.c9users.io/return-result";
 
+$.ajax({
+    url: jsonURL,
+    dataType:'json',
+    type: 'post',
+    data: yourForm.serialize(),
+    success:function(response){
+                
+        var dp = new DayPilot.Calendar("dp");
+        
+        // behavior and appearance
+        dp.cssClassPrefix = "calendar_white";
+        // view
+        dp.startDate = "2014-02-24";
+        dp.days = 5;
+        dp.headerDateFormat = "dddd"; // day of week, long format (e.g. "Monday")
+        
+        dp.events.list = response;
+        dp.init();
+    }
+});
 
-
-
-var dp = new DayPilot.Calendar("dp");
-
-// behavior and appearance
-dp.cssClassPrefix = "calendar_white";
-// view
-dp.startDate = "2014-02-24";
-dp.days = 5;
-dp.headerDateFormat = "dddd"; // day of week, long format (e.g. "Monday")
-
-dp.events.list = arrOfSchedule;
-dp.init();
