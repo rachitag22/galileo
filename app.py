5785d8c6bc3bb4056e5609aba7031df5afc895b2
Rachit Agarwal <rachitagarwal0202@gmail.com>
2017-01-29 16:19:46 +0000

    A lot more

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask
 from flask import Flask, request
 from flask import render_template
@@ -6,6 +7,8 @@ from flask import g
 from flask import redirect
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def main():
@@ -15,4 +18,18 @@ def main():
 def results():
     return render_template('results.html')
     
+@app.route('/test')
+def test():
+    return render_template('test.html')
+    
+@app.route('/compute-result',methods=['POST'])
+def compute():
+    jsdata = str(request.form)
+    print jsdata 
+    return jsdata
+    
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+
+if __name__ == '__main__':
+    app.run()
+    app.debug = True
--- lasjdflaskjf.py
+++ lasjdflaskjf.py
@@ -1,9 +1,13 @@
 import requests
 import json
 
-string = "arr = ["
 request = requests.get("http://api.umd.io/v0/map/buildings")
 parsedJson = request.json()
-for buildingInfo in parsedJson:
-    string += buildingInfo["name"] + ", "
-print string
+listOfClasses = ""
+
+for departmentInfo in parsedJson:
+    parsedJson = request.json()
+    listOfClasses += departmentInfo["name"] + "\",\""
+        
+
+print listOfClasses
